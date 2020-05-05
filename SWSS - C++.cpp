081dc2c53a208c607581bdad2c46e95656612c35
LachlanWilson <49221904+LachlanWilson@users.noreply.github.com>
2020-05-05 11:42:32 +1000

    changed for better use with arduino

--- SWSS - C++.cpp
+++ SWSS - C++.cpp
@@ -1,36 +1,53 @@
 // SWSS - C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include "AS726X.h"
 
-#include "pch.h"
-#include <iostream>
-#include <string>
+AS726X sensor;
 
-void DataRecording(int SensorInfo){
-	printf("%d", SensorInfo);
-		//with open('Data.txt', 'a') as the_file : the_file.write(wasteType + " | " + str(SensorInfo) + '\n')
+void setup() {
+  Wire.begin();
+  Serial.begin(115200);
+
+  sensor.begin();
 }
 
 int Sensor() {
-	int SensorData = 5;
-		//SensorData = Sensor()
-	return SensorData;
+  //Colour Readings
+  int Vi = sensor.getViolet();
+  int Bl = sensor.getBlue();
+  int Gr = sensor.getGreen();
+  int Ye = sensor.getYellow();
+  int Or = sensor.getOrange();
+  int Re = sensor.getRed();
+
+  int SensorData[6] = {Vi,Bl,Gr,Ye,Or,Re};
+  
+  return SensorData;
 }
 
 void MechElements(int SensorInfo) {
-	printf("working");
+
+}
+
+int DataInterprate(int sensorData[6]) {
+  if (sensorData[0] == 100){
+      // example
+    return 1;
+  }else if (sensorData[1] == 50){
+    // nested if statements will be used also here, so that we can check for very specific conditions
+    return 2;
+  }
+  return 0;
 }
 
 
-int main(){
-	while (true) {
-		int SensorInfo = Sensor(); // return number
-
-			if (SensorInfo != 0) {
-			// do anything that needs to happen for processing the data
-				DataRecording(SensorInfo);
-				MechElements(SensorInfo);
-			}
-		}
-	}
-
-
+void loop(){
+  while (true) {
+    int sensorData = Sensor(); // return number
+    int SensorInterp = DataInterprate(sensorData);
+    if (SensorInterp != 0) {
+      // do anything that needs to happen for processing the data
+      MechElements(SensorInterp);
+    }
+  }
+}
